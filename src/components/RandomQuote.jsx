a00913cd6686e6d3fea5978398074ecfb5ee0257
Rohit Tiwari <110346246+tiwaritech18@users.noreply.github.com>
2024-03-19 18:41:47 +0530

    Update RandomQuote.jsx

--- src/components/RandomQuote.jsx
+++ src/components/RandomQuote.jsx
@@ -1,7 +1,7 @@
 import { IoMdQuote } from "react-icons/io"
 import quotes from "../features/quotes"
 import { useState } from "react"
-
+import Container from "./Container"
 const RandomQuote = () => {
   const [randomQuote, setRandomQuote] = useState(() => {
     const randomIndex = Math.floor(Math.random() * quotes.length)
@@ -10,12 +10,14 @@ const RandomQuote = () => {
 
 
   return (
+<Container>
     <div className="pt-16 pb-8">
       <p className="flex max-w-80">{randomQuote.quote}
         <IoMdQuote className="text-pink-600"/>
       </p>
         <p className="italic ">- {randomQuote.author}</p>
     </div>
+<Container/>
   )
 }
 
